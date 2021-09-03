b65e6127203958d3795353b8df58d077e7556cb6
KristaB77 <87663631+KristaB77@users.noreply.github.com>
2021-09-03 12:51:43 +0300

    Add files via upload

---
+++ WorldNewsFeedFinal/WorldNewsFeedFinal/Controllers/DetailViewController.swift
@@ -0,0 +1,94 @@
+//
+//  DetailViewController.swift
+//  WorldNewsFeedFinal
+//
+//  Created by krista.bolsakova on 26/08/2021.
+//
+
+import UIKit
+import CoreData
+@objc extension  UIViewController {
+    func basicAlert(title: String?, message: String?){
+        
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+}
+
+class DetailViewController: UIViewController {
+    var savedItems = [Items]()
+    var context:NSManagedObjectContext?
+    var webURLString = String()
+    var titleString = String()
+    var contentString = String()
+    var newsImage: UIImage?
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var newsImageView: UIImageView!
+    @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var readButton: UIButton!
+    @IBOutlet weak var savedButton: UIButton!
+    
+    
+
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        readButton.layer.cornerRadius = 12
+        readButton.tintColor = .label
+        
+        self.title = "Article"
+        
+        titleLabel.text = titleString
+        contentTextView.text = contentString
+        newsImageView.image = newsImage
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        context = appDelegate.persistentContainer.viewContext
+    }
+
+
+func saveData(){
+        do{
+            try context?.save()
+           basicAlert(title: "Saved!", message: "Find saved article in Saved tab bar.")
+            
+        }catch{
+            print(error.localizedDescription)
+            
+        }
+ 
+    }
+
+    
+    @IBAction func savedButtonTapped(_ sender: Any) {
+        
+        let newItem = Items(context: self.context!)
+        newItem.newsTitle = titleString
+        newItem.newsContent = contentString
+        newItem.url = webURLString
+        
+        guard let imageData: Data = newsImage?.pngData() else{
+        return
+    }
+        if imageData.isEmpty{
+            newItem.image = imageData
+        }
+        self.savedItems.append(newItem)
+        saveData()
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination: WebViewController = segue.destination as! WebViewController
+        destination.urlString = webURLString
+    }
+
+
+}
+
+
+
+
